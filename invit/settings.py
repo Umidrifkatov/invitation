c038e02ff3706d12de04336c6a5dc2c41bb0c9db
Your Name <you@example.com>
2023-08-29 19:27:02 +0500

    r

--- invit/settings.py
+++ invit/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['static']
-STATIC_ROOT = "/home/kurbanova_sevinch/staticfiles/"
+STATIC_ROOT = "/var/www/invitation/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
