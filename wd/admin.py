94aeef2f827b6edac9db66ab81e14c49f3d570d4
Your Name <you@example.com>
2023-08-30 17:34:25 +0500

    e

--- wd/admin.py
+++ wd/admin.py
@@ -3,9 +3,13 @@ from .models import Visitor
 
 
 class VisitorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'seating_place', 'unique_identifier',)
+    list_display = ('name', 'seating_place', 'custom_method',)
     search_fields = ('name', 'seating_place')  # Add fields for search
     list_filter = ('seating_place',)  # Add a filter for seating place
+    
+    def custom_method(self, obj):
+        return f"umidsevinch.invitation.uz/{obj.unique_identifier}"
+    custom_method.short_description = 'Custom Display'
 
 admin.site.register(Visitor, VisitorAdmin)
 
